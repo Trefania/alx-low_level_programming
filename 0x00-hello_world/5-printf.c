3d6de5e184d8a7e69d82f1f68735ba35c8263bc7
Trefania <tarirotrefania@gmail.com>
2022-06-30 14:47:59 -0700

    Test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - prints the size of various types
- *
- * * Return: 0 if exited properly, non-zero otherwise
- *
- * */
-
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
 }
