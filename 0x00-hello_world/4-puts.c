e9f1283301b7c676a6de23ba3d1013e3ccc0ced0
Trefania <tarirotrefania@gmail.com>
2022-06-30 16:33:16 -0700

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,18 +4,20 @@
 
 /**
  *
- * * main - prints the size of various types
+ *  * main - Entry point
  *
- * * Return: 0 if exited properly, non-zero otherwise
+ *   *
  *
- * */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
