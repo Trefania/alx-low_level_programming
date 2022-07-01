5ba0557bfd976533b01da1bd7eea2d75b699a551
Trefania <tarirotrefania@gmail.com>
2022-07-01 16:34:57 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,18 +11,16 @@
 
 int main(void)
 {
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-	
-	{
-	if (ch != 'q' && ch != 'e')
-	{
-	putchar(ch);
-	}
-	}
-	putchar(10); /* this is an ascii code for new line*/
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)	
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
 
