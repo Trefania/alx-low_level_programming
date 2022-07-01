aa7364dd05fc17c47fb6f7dade14936c981909e4
Trefania <tarirotrefania@gmail.com>
2022-07-01 14:38:09 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,26 @@
-#include <stdio.h>                                                                                                                                                              
-                                                                                                                                                                                
+#include <stdio.h>
 
-/**                                                                                                                                                                             
- *  main -> assign a random number to the variable n each time it is executed
- *  and print the last digit of the number stored in the variable n
- *  Return : always 0
-*/      
+/**
+ *  *main -> assign a random number to the variable n each time it is executed
+ *
+ *   *and print the last digit of the number stored in the variable n
+ *
+ *    *Return: Always 0 (Success)
+ *
+ */
 
-int main(void)                                                                                                   
+int main(void)
 {
-	char  ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch);
-		}
+	if (ch != 'q' && ch != 'e')
+	{
+		putchar(ch);
 	}
+	}
+	putchar(10); /* this is an ascii code for new line*/
 
-	putchar(10);
- 
-	return (0);                                                                                                                                                             
-
+	return (0);
 }
+
