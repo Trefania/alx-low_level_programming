6c7525773be67f95b7f4c6b384642f4de18795c2
Trefania <tarirotrefania@gmail.com>
2022-07-03 15:39:04 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
-
 /**
+ *  *main - prints to the screen using puts function
  *
- * * main - prints exactly "and that piece of art is useful"
+ *  *Return: If successful the exit status is zero
  *
- * * - Dora Korpar, 2015-10-19",
- *
- * * followed by a new line, to the standard error.
- *
- * * Return: Always 0 (Success)
- *
- * */
+ */
 
 int main(void)
 
-{
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 
 }
+
