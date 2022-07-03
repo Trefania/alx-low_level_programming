dd0ff94aa46fd14ece2646815b4b7c351fd783ce
Trefania <tarirotrefania@gmail.com>
2022-07-03 12:47:29 -0700

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *  *main - prints to the screen using puts function
+ *
+ *  *Return: If successful the exit status is zero
+ *
+ */
+
+int main(void)
+{
+int i, j;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(j);
+putchar(i);
+if (j == 57) && (i == 56))
+break;
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+
+return (0);
+}
