875baf2c9ad7c3b0f1ce982ab3d9c91c93268b0e
Trefania <tarirotrefania@gmail.com>
2022-07-04 04:38:48 -0700

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,14 @@
 
 int main(void)
 {
-	int i, j;
+int i, j;
 
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = i + 1; j <= 57; j++)
 		{
-
-			putchar(j);
 			putchar(i);
+			putchar(j);
 			if ((j == 57) && (i == 56))
 				break;
 			putchar(44);
