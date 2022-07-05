8d80ca4bfcbd6a1d0e176a922fdb9e8e1d140cff
Trefania <tarirotrefania@gmail.com>
2022-07-05 07:12:04 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print _alphabet -> prints the lowercase alphabets
+ */
+
+void print _alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
