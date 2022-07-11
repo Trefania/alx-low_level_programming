b71ac9656038d72f34b14c430c0b886fd94c6d52
Trefania <tarirotrefania@gmail.com>
2022-07-11 14:17:38 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - Randomly generate pass
+ * Decription: Program to randomly generates password.
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
