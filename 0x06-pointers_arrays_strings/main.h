7b62652229804337fd00c4baf8d9045dbb040590
Trefania <tarirotrefania@gmail.com>
2022-07-13 11:19:18 -0700

    more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
+#innclude "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code for the task.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s1[98] = "Hello ";
+char s2[] = "World\n";
+char *ptr;
 
-#endif
+printf("%s\n", s1);
+printf("%s", s2);
+ptr = _strcat(s1, s2);
+printf("%s", s1);
+printf("%s", s2);
+printf("%s", ptr);
+return (0);
+}
