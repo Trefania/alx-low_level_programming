f7fc61765c658f8b36ca90e15e869d743d8c52f1
Trefania <107484364+Trefania@users.noreply.github.com>
2022-08-02 00:51:23 +0200

    changes were made

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,27 @@
 #define FILE_DOG
 
 /**
- * struct dog - is the structure dog
- * @name - is the attribute of dog's name
- * @age - attribute of dog's age
- * @owner - attribute of dog's owner
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
  */
 
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
