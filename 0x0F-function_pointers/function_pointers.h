bf663a637c6768a957dad7eb4c3392b8693e1207
Trefania <tarirotrefania@gmail.com>
2022-08-03 13:16:14 -0700

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
-  * Desc: header file cointaining all prototypes
-  *       used in  the 0x0F-function_pointer directory 
-*/
-
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
