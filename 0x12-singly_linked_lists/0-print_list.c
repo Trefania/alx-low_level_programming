0a31b554f5f44ddf82f4f91dabcbc5b46f73b5d9
Trefania <tarirotrefania@gmail.com>
2022-08-11 15:15:23 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,17 +4,16 @@
 
 size_t print_list(const list_t *h)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        while (h != 0)
-        {
-        if (h -> str == NULL)
-                printf("[%d] %s\n", 0, "(nil)");
-        else
-                printf("[%u] %s\n", h ->len, h -> str);
-
-        h = h -> next;
-                        count++;
-        }
-        return (count);
+	while (h != 0)
+	{
+		if (h -> str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", h ->len, h -> str);
+		h = h -> next;
+		count++;
+	}
+	return (count);
 }
