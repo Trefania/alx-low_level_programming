479a4f761cc28fd0ae50021a58a386f36d060499
Trefania <tarirotrefania@gmail.com>
2022-08-22 16:18:22 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,8 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	size_t reading, writing;
 	char *buffer;
 
-	if(filename == NULL)
-		return 0;
+	if (filename == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
