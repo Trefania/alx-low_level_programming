49d0b8aa8106aae9734cdc08ddba28429377ed72
Trefania <tarirotrefania@gmail.com>
2022-09-29 13:33:15 -0700

    adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint_end - adds a new node to the end of list
  * @head: double pointer to head of node
- * @n: node data
+ * @n: node data.
  * Return: new node
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
