10a973cbbadbf5478ded11c7180d2bac1b6ab695
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-30 23:50:16 +0200

    binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches for a value in a sorted array
+ *                 of integers using binary search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: value index or -1 if not present or the array is NULL
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, low, high;
+	int b;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (x = low; b <= high; b++)
+			printf("%i%s", array[b], b == high ? "\n" : ", ");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid)
+	}
+
+	return (-1);
+}
